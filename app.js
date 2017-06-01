ecfd99b2aa67a1dfb659c0d3da2cd7f87ec6f8ea
Haley Tortorella <hdtort@gmail.com>
2017-06-01 15:28:40 -0700

    added content

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Project = [
+    {
+       title: 'First Project',
+       date: '01/2/2017',
+       img: 'filepath',
+       url: 'www.com',
+    },
+   
+    {
+       title: 'Second Project',
+       date: '12/3/2017',
+       img: 'filepath',
+       url: 'www.com',
+
+    },
+];
+
+
