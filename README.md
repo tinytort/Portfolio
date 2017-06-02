13fae0196336b282ddb355897c685711ea5148ff
Haley Tortorella <hdtort@gmail.com>
2017-06-01 21:12:30 -0700

    script set up and CSS addded. Iconmoon font included

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Portfolio
+
+## User Stories
+As a user, I want to be able to find Haley's work with ease.
+As a user, I want this site to be mobile optimized.
+As a user, I want to find relevant details like contact information, so I can contact Haley.
+
+As a developer, I want to write DRY code so that it can be easily understood.
+As a developer, I wand to use SMACSS to organize my CSS. 
---
+++ project.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var projects = [
+    {
+       title: 'First Project',
+       date: '01/2/2017',
+       img: 'filepath',
+       url: 'www.com',
+    },
+   
+    {
+       title: 'Second Project',
+       date: '12/3/2017',
+       img: 'filepath',
+       url: 'www.com',
+
+    },
+    
+    {
+        title: 'Third Project',
+        date: '02/03/2017',
+        img: 'filepath',
+        url: 'www.com',
+    }
+];
+
+
