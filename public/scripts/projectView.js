233df7c1b2748de6e32ffd5ce6792bcaed6d8868
Haley Tortorella <hdtort@gmail.com>
2017-06-08 16:10:34 -0700

    added a server

---
+++ public/scripts/projectView.js
@@ -0,0 +1,18 @@
+'use strict';
+// Tabs
+var viewObj = {};
+$('.tabContent').hide();
+
+
+viewObj.handleNav = function () {
+    $('#navBar').on('click', 'li', function () {
+        event.preventDefault();
+        $('.tabContent').hide();
+        $(`div[data-tab=${$(this).attr('data-tab')}]`).show();
+    });
+};
+
+
+
+
+viewObj.handleNav();
